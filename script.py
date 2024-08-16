38a0591224cd2b0fce9c6f8aa72f3bf91fa8a992
DHRUV BHALODIA <dhruvcoder007@gmail.com>
2024-08-16 19:30:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,23 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
-def set_chrome_options() -> Options:
-    """Sets chrome options for Selenium.
-    Chrome options for headless browser is enabled.
-    """
-    chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-    return chrome_options
-
-
-driver = webdriver.Chrome(options=set_chrome_options())
+driver = webdriver.Chrome()
 
 driver.get("https://leetcode.com/u/daivik_hirpara/")
 
