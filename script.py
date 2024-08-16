615d66c861bda320e0d5d3844d006feb881ce63d
DHRUV BHALODIA <dhruvcoder007@gmail.com>
2024-08-16 19:28:11 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,348 +1,41 @@
-import requests
-import pandas as pd
-import re
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import json
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# Instructions are taken from https://nander.cc/using-selenium-within-a-docker-container
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-def get_codechef_profile_image(username):
-    url = username
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        profile_image_element = soup.find('img', class_='profileImage')
-        if profile_image_element:
-            profile_image_url = profile_image_element['src']
-            profile_image_url = urljoin(url, profile_image_url)
-            return "codechef.jpg"
-        else:
-            return "Profile image not found for this user"
-    else:
-        return "User profile not found or inaccessible"
 
-def get_leetcode_profile_image(username):
-    url = username
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        profile_image_element = soup.find('img', class_='h-20 w-20 rounded-lg object-cover')
-        if profile_image_element:
-            profile_image_url = profile_image_element['src']
-            profile_image_url = urljoin(url, profile_image_url)
-            return profile_image_url
-        else:
-            return "Profile image not found for this user"
-    else:
-        return "User profile not found or inaccessible"
+def set_chrome_options() -> Options:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/google-chrome"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
 
-def get_codeforces_profile_image(username):
-    url = username
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        profile_image_element = soup.find('img', style='margin:auto;vertical-align:middle;display:inline;')
-        if profile_image_element:
-            profile_image_url = profile_image_element['src']
-            profile_image_url = urljoin(url, profile_image_url)
-            return profile_image_url
-        else:
-            return "Profile image not found for this user"
-    else:
-        return "User profile not found or inaccessible"
 
-def get_codechef_rating(username):
-    url = username
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=set_chrome_options())
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(url)
-    time.sleep(5)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    rating_element = soup.find('div', class_='rating-number')
-    paths = soup.find_all('path')
-    if rating_element:
-        return int(re.search(r'\d+', rating_element.text.strip()).group())
-    else:
-        return "-1"
+driver.get("https://leetcode.com/u/daivik_hirpara/")
 
-def get_leetcode_rating(username):
-    url = username
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    
+for _ in range(3):  # Retry 3 times
     try:
-        driver.get(url)
-        class_name = 'text-label-1.dark\\:text-dark-label-1.flex.items-center.text-2xl'
-        print(class_name)
         elements = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'div.{class_name}'))
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.text-label-1.dark\\:text-dark-label-1.flex.items-center.text-2xl'))
         )
-        for element in elements:
-            print(element.text)
-        return "-1"
-    
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return "-1"
-    
-    finally:
-        driver.quit()
+        if elements:
+            print("hii")
+            break
+    except Exception:
+        time.sleep(2)  # Wait before retrying
 
-def get_codeforces_rating(username):
-    url = username
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(url)
-    time.sleep(10)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    rating_element = soup.find_all('span', style='font-weight:bold;')
-    print(rating_element)
-    if rating_element[0]:
-        rating = rating_element[0].get_text(strip=True)
-        return int(rating)
-    else:
-        return -1
-
-def total_contest_codechef(username):
-    url = username
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        total = soup.find('div', class_='contest-participated-count')
-        return int(re.search(r'\d+', total.text.strip()).group()) if total else "Total contest not found"
-    else:
-        return "User profile not found or inaccessible"
-
-def total_contest_leetcode(username):
-    url = username
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        rating_element = soup.find_all('div', {'class': 'text-label-1 dark:text-dark-label-1 font-medium leading-[22px]'})
-        if len(rating_element) >= 2:
-            second_element_value = rating_element[1].text.strip()
-            return int(second_element_value)
-        return 0
-    else:
-        return "Failed to retrieve data"
-
-def total_contest_codeforces(username):
-    url = username
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(url)
-    time.sleep(5)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    div = soup.find('td', {'class': 'dark left'})
-    if div:
-        text = div.get_text(strip=True)
-        return int(text)
-    else:
-        return 0
-
-def get_star_rating(rating):
-    if rating < 1400:
-        return "⭐"
-    elif rating < 1600:
-        return "⭐⭐"
-    elif rating < 1800:
-        return "⭐⭐⭐"
-    elif rating < 2000:
-        return "⭐⭐⭐⭐"
-    elif rating < 2200:
-        return "⭐⭐⭐⭐⭐"
-    elif rating < 2500:
-        return "⭐⭐⭐⭐⭐⭐"
-    else:
-        return "⭐⭐⭐⭐⭐⭐⭐"
-
-def get_user_top(username):
-    url = username
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get(url)
-    time.sleep(5)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    div = soup.find('div', {'class': 'text-label-1 dark:text-dark-label-1 text-2xl'})
-    if div:
-        text = div.get_text(strip=True)
-        return str(text.strip("%"))
-    else:
-        return "Element not found"
-    driver.quit()
-
-def get_div_codeforces(rating):
-    if rating <= 1199:
-        return "Newbie"
-    elif rating <= 1399:
-        return "Pupil"
-    elif rating <= 1599:
-        return "Specialist"
-    elif rating <= 1899:
-        return "Expert"
-    elif rating <= 2099:
-        return "Candidate Master"
-    elif rating <= 2299:
-        return "Master"
-    elif rating <= 2399:
-        return "International Master"
-    elif rating <= 2599:
-        return "Grandmaster"
-    elif rating <= 2999:
-        return "International Grandmaster"
-    elif rating >= 3000:
-        return "Legendary Grandmaster"
-
-# Read input data from input Excel file
-excel_file = 'Sheet.xlsx' 
-df = pd.read_excel(excel_file)
-
-# Process CodeChef data
-output_json = 'codechef.json'
-output_data = []
-
-for _, user in df.iterrows():
-    username = ""
-    year = int(re.search(r'\d+', user['Email']).group()[:4])
-    if pd.notna(user['CodeChef ID']) and user['CodeChef ID']:
-        username = user['CodeChef ID']
-    else:
-        continue
-
-    rating = get_codechef_rating(username)
-    star = ""
-    total_contests = 0
-    try:
-        rating = int(rating)
-        star = get_star_rating(rating)
-        total_contests = total_contest_codechef(username)
-        if total_contests:
-            total_contests = int(total_contests)
-    except:
-        rating = 0
-        print("rating doesn't exist")
-    
-    output_data.append({
-        "name": user['Name (First & Last Name)'],
-        "year": year,
-        "stars": star,
-        "codechefRating": rating,
-        "totalContest": total_contests,
-        "img": get_codechef_profile_image(username),
-        "url": username
-    })
-
-# Write CodeChef data to JSON file
-with open(output_json, 'w') as json_file:
-    json.dump(output_data, json_file, indent=4)
-
-output_json = 'leetcode.json'
-output_data = []
-
-for _, user in df.iterrows():
-    username = ""
-    rating = 0
-    contests = 0
-    star = 0
-    year = int(re.search(r'\d+', user['Email']).group()[:4])
-    if pd.notna(user['LeetCode ID']) and user['LeetCode ID']:
-        username = user['LeetCode ID']
-    else:
-        continue
-
-    print(username)
-    
-    try:
-        rating = int(get_leetcode_rating(username))
-        star = "Top " + get_user_top(username) + "%"
-        contests = total_contest_leetcode(username)
-    except:
-        print("rating not exist")
-        
-    output_user_data = {
-        "name": user['Name (First & Last Name)'],
-        "year": year,
-        "id": username,
-        "stars": star,
-        "leetcodeRating": rating,
-        "totalContest": contests,
-        "img":get_leetcode_profile_image(username),
-        "url": username
-    }
-    output_data.append(output_user_data)
-    print(f"{user['Name (First & Last Name)']} {rating}")
-
-with open(output_json, 'w') as file:
-    json.dump(output_data, file, indent=4)
-
-# output_json = 'codeforces.json'
-# output_data = []
-
-# for _, user in df.iterrows():
-#     username = ""
-#     rating = 0
-#     contests = 0
-#     star = 0
-#     year = int(re.search(r'\d+', user['Email']).group()[:4])
-#     if pd.notna(user['CodeForces ID']) and user['CodeForces ID']:
-#         username = user['CodeForces ID']
-#     else:
-#         continue
-#     try:
-#         rating = get_codeforces_rating(username)
-#         contests = total_contest_codeforces(username)
-#         star = get_div_codeforces(rating)
-#     except:
-#         print("rating not exist")
-        
-#     output_user_data = {
-#         "name": user['Name (First & Last Name)'],
-#         "year" : year,
-#         "id": username,
-#         "stars": star,
-#         "codeforcesRating": rating,
-#         "totalContest": contests,
-#         "img":get_codeforces_profile_image(username),
-#         "url": username
-#     }
-#     output_data.append(output_user_data)
-#     print(f"{user['Name (First & Last Name)']} {rating}")
-
-# with open(output_json, 'w') as file:
-#     json.dump(output_data, file, indent=4)
-
-# print(f"Data written to {output_json}")
+print("Completed")
+driver.close()
